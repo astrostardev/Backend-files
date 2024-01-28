d86b39443d817313138472db7563954de5837e3e
astrostardev <153346634+astrostardev@users.noreply.github.com>
2024-01-28 22:14:47 +0530

    Add files via upload

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,126 +1,131 @@
-const catchAsyncError = require('../middlewares/catchAsyncError')
-const Course = require('../models/courseModel');
-const path = require('path'); // Import the 'path' module
+const catchAsyncError = require("../middlewares/catchAsyncError");
+const Course = require("../models/courseModel");
+const path = require("path"); // Import the 'path' module
 
-exports.createCourse = catchAsyncError(async (req, res, next)=>{
-  let images = []
-    let BASE_URL = process.env.BACKEND_URL;
-    if(process.env.NODE_ENV === "production"){
-        BASE_URL = `${req.protocol}://${req.get('host')}`
-    }
-    
-  
-    if (req.files['images']) {
-      images = req.files['images'].map(file => ({
-        file: `${BASE_URL}/uploads/courseImages/${file.originalname}`
-      }));
-    }
-    req.body.images = images;
-
-const { coursename } = req.body;
-console.log('hi', coursename);
-
-const existingCourse = await Course.findOne({ coursename: { $regex: new RegExp(coursename, 'i') } });
-
-if (existingCourse) {
-    // Product already exists
-    return res.status(409).json({
-        success: false,
-        message: 'Course already registered',
-    });
-}
-
-   console.log('responds',req.body);
-    const course = await Course.create(req.body);
-    res.status(201).json({
-        success: true,
-     course
-    })
-});
-
-
-
-exports.showCourses = catchAsyncError(async(req,res,next)=>{
-    const courses = await Course.find();
-  
-    // Set Content-Type to application/json
-    res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  
-    res.status(200).json({
-        success: true,
-        courses
-    }); 
-    }) 
-    exports.getCourse = catchAsyncError(async(req,res,next)=>{
-      const course = await Course.findById(req.params.id);
-    
-      // Set Content-Type to application/json
-      res.setHeader('Content-Type', 'application/json; charset=utf-8');
-    
-      res.status(200).json({
-          success: true,
-          course
-      }); 
-      }) 
-      exports.updateCourse = catchAsyncError(async (req, res, next) => {
-        let course = await Course.findById(req.params.id);
-    let images = []
-      
-        if(req.body.imagesCleared === 'false' ) {
-          images = course.images;
-      }
-        
-        let BASE_URL = process.env.BACKEND_URL;
-    if(process.env.NODE_ENV === "production"){
-        BASE_URL = `${req.protocol}://${req.get('host')}`
-    }
-    if(req.files.length > 0) {
-        req.files.forEach( file => {
-            let url = `${BASE_URL}/uploads/courseImages/${file.originalname}`;
-            images.push({ image: url })
-        })
-    }
-
-
-    req.body.images = images;
-    if(!course) {
-      return res.status(404).json({
-          success: false,
-          message: "Course not found"
-      });
+exports.createCourse = catchAsyncError(async (req, res, next) => {
+  let images = [];
+  let BASE_URL = process.env.BACKEND_URL;
+  if (process.env.NODE_ENV === "production") {
+    BASE_URL = `${req.protocol}://${req.get("host")}`;
   }
 
-const { coursename } = req.body;
-console.log('hi', coursename);
+  if (req.files.length > 0) {
+    req.files.forEach((file) => {
+      let url = `${BASE_URL}/uploads/courseImages/${file.originalname}`;
+      images.push({ image: url });
+    });
+  }
+  req.body.images = images;
 
-const existingCourse = await Course.findOne({ coursename: { $regex: new RegExp(coursename, 'i') } });
+  const { coursename } = req.body;
+  console.log("hi", coursename);
 
-if (existingCourse) {
+  const existingCourse = await Course.findOne({
+    coursename: { $regex: new RegExp(coursename, "i") },
+  });
+
+  if (existingCourse) {
     // Product already exists
     return res.status(409).json({
-        success: false,
-        message: 'Course already registered',
+      success: false,
+      message: "Course already registered",
     });
-}
+  }
 
-  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-  })
+  console.log("responds", req.body);
+  const course = await Course.create(req.body);
+  res.status(201).json({
+    success: true,
+    course,
+  });
+});
+
+exports.showCourses = catchAsyncError(async (req, res, next) => {
+  const courses = await Course.find();
+
+  // Set Content-Type to application/json
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
 
   res.status(200).json({
-      success: true,
-      course
-  })
-      
-     
+    success: true,
+    courses,
+  });
+});
+exports.getCourse = catchAsyncError(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  // Set Content-Type to application/json
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+
+  res.status(200).json({
+    success: true,
+    course,
+  });
+});
+exports.updateCourse = catchAsyncError(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  let images = [];
+  if (req.body.imagesCleared === "false") {
+    images = course.images;
+  }
+
+  let BASE_URL = process.env.BACKEND_URL;
+  if (process.env.NODE_ENV === "production") {
+    BASE_URL = `${req.protocol}://${req.get("host")}`;
+  }
+
+  if (req.files.length > 0) {
+    req.files.forEach((file) => {
+      let url = `${BASE_URL}/uploads/courseImages/${file.originalname}`;
+      images.push({ image: url });
+    });
+  }
+
+  req.body.images = images;
+
+  if (!course) {
+    return res.status(404).json({
+      success: false,
+      message: "Course not found",
+    });
+  }
+
+  const { coursename } = req.body;
+
+  console.log("hi", coursename);
+
+  if (coursename !== course.coursename) {
+    // Product name has been modified, check for existence
+    const existingCourse = await Course.findOne({
+      coursename: { $regex: new RegExp(coursename, "i") },
+    });
+
+    if (existingCourse) {
+      // Product with the new name already exists
+      return res.status(409).json({
+        success: false,
+        message: "Course already registered",
       });
-    exports.deleteCourse = catchAsyncError(async (req, res, next) => {
-        const course = await  Course.findById(req.params.id);
-    
-        await course.deleteOne();
-        res.status(200).json({
-          sucess: true,
-    
-        })
-      })
+    }
+  }
+
+  // Update the product details
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    course,
+  });
+});
+exports.deleteCourse = catchAsyncError(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  await course.deleteOne();
+  res.status(200).json({
+    sucess: true,
+  });
+});
--- controllers/productController.js
+++ controllers/productController.js
@@ -9,29 +9,26 @@ exports.createProduct = catchAsyncError(async (req, res, next) => {
     BASE_URL = `${req.protocol}://${req.get("host")}`;
   }
  
-  if (req.files['images']) {
-    images = req.files['images'].map(file => ({
-      file: `${BASE_URL}/uploads/ProductImages/${file.originalname}`
-    }));
+  if (req.files.length > 0) {
+    req.files.forEach((file) => {
+      let url = `${BASE_URL}/uploads/ProductImages/${file.originalname}`;
+      images.push({ image: url });
+    });
   }
   req.body.images = images;
 
   const { productname } = req.body;
-console.log('hi', productname);
+  console.log('hi', productname);
 
-const existingProduct = await Product.findOne({ productname: { $regex: new RegExp(productname, 'i') } });
+  const existingProduct = await Product.findOne({ productname: { $regex: new RegExp(productname, 'i') } });
 
-if (existingProduct) {
+  if (existingProduct) {
     // Product already exists
     return res.status(409).json({
         success: false,
         message: 'Product already registered',
     });
 }
-
-
-
-
   req.body.images = images;
   console.log(req.body);
   const product = await Product.create(req.body);
@@ -65,57 +62,64 @@ exports.getProduct = catchAsyncError(async (req, res, next) => {
 });
 exports.updateProduct = catchAsyncError(async (req, res, next) => {
   let product = await Product.findById(req.params.id);
-  
-  let images = []
-  if(req.body.imagesCleared === 'false' ) {
+
+  let images = [];
+  if (req.body.imagesCleared === 'false') {
     images = product.images;
-   }
-  
+  }
+
   let BASE_URL = process.env.BACKEND_URL;
-if(process.env.NODE_ENV === "production"){
-  BASE_URL = `${req.protocol}://${req.get('host')}`
-}
-if(req.files.length > 0) {
-  req.files.forEach( file => {
+  if (process.env.NODE_ENV === 'production') {
+    BASE_URL = `${req.protocol}://${req.get('host')}`;
+  }
+
+  if (req.files.length > 0) {
+    req.files.forEach((file) => {
       let url = `${BASE_URL}/uploads/ProductImages/${file.originalname}`;
-      images.push({ image: url })
-  })
-}
+      images.push({ image: url });
+    });
+  }
 
+  req.body.images = images;
 
-req.body.images = images;
-if(!product) {
-return res.status(404).json({
-    success: false,
-    message: "Product not found"
-});
-}
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found',
+    });
+  }
 
-const { productname } = req.body;
-console.log('hi', productname);
+  const { productname } = req.body;
 
-const existingProduct = await Product.findOne({ productname: { $regex: new RegExp(productname, 'i') } });
+  console.log('hi', productname);
 
-if (existingProduct) {
-    // Product already exists
-    return res.status(409).json({
+  if (productname !== product.productname) {
+    // Product name has been modified, check for existence
+    const existingProduct = await Product.findOne({
+      productname: { $regex: new RegExp(productname, 'i') },
+    });
+
+    if (existingProduct) {
+      // Product with the new name already exists
+      return res.status(409).json({
         success: false,
         message: 'Product already registered',
-    });
-}
+      });
+    }
+  }
 
-product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-new: true,
-runValidators: true
-})
+  // Update the product details
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-res.status(200).json({
-success: true,
-product
-})
-
-
+  res.status(200).json({
+    success: true,
+    product,
+  });
 });
+
 
 exports.deleteProduct = catchAsyncError(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
