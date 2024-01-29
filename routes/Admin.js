cb5957261e2d7ac0d61ef40403265d453a50552e
astrostardev <153346634+astrostardev@users.noreply.github.com>
2024-01-29 12:34:12 +0530

    Add files via upload

--- routes/Admin.js
+++ routes/Admin.js
@@ -1,30 +1,10 @@
 const express = require('express')
 const{registerAdmin, loginAdmin, logoutAdmin} = require('../controllers/adminController')
 const router = express.Router()
-const jwt = require('jsonwebtoken')
 
-
-
-const verification = async(req, res, next)=>{
-    try{
-      let token = req.header("Authorization")
-      if(token && token.startsWith("Bearer ")){
-        token = token.slice(7,token.length).trimLeft();
-        const verified = jwt.verify(token,process.env.JWT_SECRET)
-        req.user = verified
-        console.log('verification',verified);
-        next()
-      }
-      else{
-        res.status(403).send("Access denied")
-      }
-    }catch(err){
-       res.status(400).json({msg:err.message})
-    }
-  }
 router.route('/admin/register').post(registerAdmin)
 router.route('/admin/login').post(loginAdmin);
-router.route('/admin/logout').get(verification,logoutAdmin);
+router.route('/admin/logout').get(logoutAdmin);
 
 
 module.exports = router
